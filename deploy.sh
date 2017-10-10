8b09d60801b0b624f382dc0a199c135f03abb62e
walisonjose <walison.josededeus@gmail.com>
2017-10-10 09:37:40 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/
-#git pull
+cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/ 
+pwd
+                   #git pull
+#
+#if [ $1 = 'dev' ]
+#then
+#    composer install
+#else
+#    composer install --no-dev
+#fi
 
-if [ $1 = 'dev' ]
-then
-    composer install
-else
-    composer install --no-dev
-fi
-
-sh /home/migracao.redehumanizasus.net/integracao/wp-rhs/compile-sass.sh
-cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/public
-wp rewrite flush
-wp language core update
-cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/migration-scripts
-php /home/migracao.redehumanizasus.net/integracao/wp-rhs/rhs_migrations.php all
+#sh /home/migracao.redehumanizasus.net/integracao/wp-rhs/compile-sass.sh
+#cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/public
+#wp rewrite flush
+#wp language core update
+#cd /home/migracao.redehumanizasus.net/integracao/wp-rhs/migration-scripts
+#php /home/migracao.redehumanizasus.net/integracao/wp-rhs/rhs_migrations.php all
