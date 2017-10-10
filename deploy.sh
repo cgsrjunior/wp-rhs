f497625a5bf62d402c18dd733c10849071e70e22
walisonjose <walison.josededeus@gmail.com>
2017-10-10 14:54:51 -0300

    Integracao Contínua

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ fi
 sh compile-sass.sh
 cd public
 
-mv htaccess-sample htaccess
-mv wp-config-sample.php wp-config.php
+#mv htaccess-sample htaccess
+#mv wp-config-sample.php wp-config.php
 
 wp rewrite flush
 wp language core update
-cd migration-scripts
+cd ~/integracao/wp-rhs/migration-scripts
 php rhs_migrations.php all
