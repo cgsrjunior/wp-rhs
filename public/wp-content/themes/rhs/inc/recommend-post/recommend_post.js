f5a182b00a9acfc7600a11572788a14cdb5ca305
Pedro Naves <oi@pedronaves.com.br>
2018-03-14 16:04:56 -0300

    adicionando opção para selecionar várias pessoas para indicação (ref. #192)

--- public/wp-content/themes/rhs/inc/recommend-post/recommend_post.js
+++ public/wp-content/themes/rhs/inc/recommend-post/recommend_post.js
@@ -1,59 +1,59 @@
-jQuery( function( $ ) {
-    var input_recommend_post = '#input-recommend-post';
-    
-    $(input_recommend_post).autocomplete({
-        serviceUrl: vars.ajaxurl,
-        type: 'POST',
-        dataType: 'json',
-        paramName: 'string',
-        params : {
-            action: 'show_people_to_recommend',
+var $ = jQuery;
+var input_recommend_post = '#input-recommend-post';
+var button_to_submit_recommend = '#submit-recommend';
+
+$(function() {
+    var ms = $(input_recommend_post).magicSuggest({
+        data: vars.ajaxurl,
+        dataUrlParams: { 
+            action: 'show_people_to_recommend'
         },
-        minChars: 3,
-        onSelect: function (suggestion) {
-            swal({
-                title: "Deseja indicar esse post?",
-                text: "A indicação será enviada para " + suggestion.value,
-                type: "info",
-                showCancelButton: true,
-                confirmButtonClass: "btn-success",
-                confirmButtonText: "Sim, enviar!",
-                cancelButtonText: "Cancelar",
-                closeOnConfirm: false,
-                closeOnCancel: false
-            },
-            function (isConfirm) {
-                var not_sent_title = "Não enviado!";
-                if (isConfirm) {
-                    $.ajax({
-                        async: false,
-                        type: "POST",
-                        dataType: "json",
-                        url: vars.ajaxurl,
-                        data: {
-                            action: 'recommend_the_post',
-                            user_id: suggestion.data,
-                            post_id: $(input_recommend_post).data('post-id')
-                        },
-                        success: function (data) {                            
-                            if(data.msgErr) {
-                                swal(not_sent_title, data.msgErr, "error");
-                            } else if(data.messages.success && data.user.sent_name) {
-                                swal("Enviado!", "Indicação enviada com sucesso para " + data.user.sent_name, "success");
-                            } else {
-                                swal(not_sent_title, "Tente novamente mais tarde!", "error");
-                            }                                                    
-                        },
-                        error: function (data) {
-                            swal(not_sent_title, "Sua indicação não foi enviada.", "error");
-                        }
-                    });
-                } else {
-                    swal("Cancelado", not_sent_title, "error");
-                }
-            });
-            
-            return false;
+        method: 'post',
+        valueField: 'user_id',
+        placeholder: 'Insira o nome do usuário',
+        allowFreeEntries: false,
+        maxSuggestions: 10,
+        noSuggestionText: 'Sem sugestões para {{query}}',
+        renderer: function(data){
+            return '<div style="padding: 5px; overflow:hidden;">' +
+                '<div style="float: left; margin-left: 5px">' +
+                    '<div style="font-weight: bold; color: #333; font-size: 14px; line-height: 13px">' + data.name + '</div>' +
+                '</div>' +
+            '</div><div style="clear:both;"></div>';
         }
+    });
+
+    $(button_to_submit_recommend).click(function () {
+        var id_post = $(this).data('post-id');
+        var not_sent_title = "Não enviado!";
+        var json_users = JSON.stringify(ms.getSelection());
+        $(button_to_submit_recommend).text('Enviando...');
+
+        $.ajax({
+            type: 'POST',
+            url: vars.ajaxurl,
+            dataType: 'json',
+            data: {
+                'post_id': id_post,
+                'users': json_users,
+                'action': 'recommend_the_post'
+            }, success: function (result) {
+                $(button_to_submit_recommend).text('Enviado');
+                if(result.msgErr) {
+                    swal(not_sent_title, result.msgErr, "error");
+                } else if(result.messages.success && result.user.sent_name) {
+                    swal("Enviado!", "Indicação enviada com sucesso", "success");
+                    ms.clear();
+                } else {
+                    swal(not_sent_title, "Tente novamente mais tarde!", "error");
+                }
+               
+            },
+            error: function () {
+                swal(not_sent_title, "Sua indicação não foi enviada.", "error");
+                $(button_to_submit_recommend).text('Enviar');
+            }
+        });
+    
     });
 });
